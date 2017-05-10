8ff93c2208b58909dab24db84080cf0aee84f28b
ClintonKing <clintonjoking@gmail.com>
2017-05-10 19:07:47 -0400

    Smarter order of operations, better error messaging.

--- scrape_album.py
+++ scrape_album.py
@@ -9,38 +9,39 @@ from models.band import bandPage
 from models.album import albumPage
 
 #use generated urls for album pages to scrape album and song info
-def scrape_album(album_ext):
-    url = BASE_URL + album_ext
-    response = requests.get(url)
-    if response:
-        album = albumPage(response.content)
-        print(album.artist)
+def scrape_albums(page):
+    all_albums = []
+    for album_ext in page.albums:
+        url = BAND_URL + album_ext
+        response = requests.get(url)
+        if response:
+            album = albumPage(response.content)
+            all_albums.append(album)
+    for album in all_albums:
+        print(album.title)
+
 
 
 #scrape band's page for urls to individual album pages
 def scrape_index():
-    url = BASE_URL + '/music'
-    response = requests.get(url)
-    if response:
+    url = BAND_URL
+    try:
+        response = requests.get(url)
         page = bandPage(response.content)
-
-        for album_ext in page.albums:
-            scrape_album(album_ext)
-
-
-    else:
-        print('Could not find that band, sorry.')
+        scrape_albums(page)
+    except requests.exceptions.MissingSchema:
+        print('Sorry, that url does not seem to exist.')
 
 
 
 
-#runs app when provided band name
+
+#runs app when provided bandcamp url
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        print('band name required')
+        print('bandcamp url required')
         sys.exit()
 
-    band = str(sys.argv[1])
-    global BASE_URL
-    BASE_URL = 'https://' + band + '.bandcamp.com'
+    global BAND_URL
+    BAND_URL = str(sys.argv[1])
     scrape_index()
