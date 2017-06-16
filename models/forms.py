aeeeb0800c227e7ef5581d7266f21839d9512ee9
ClintonKing <clintonjoking@gmail.com>
2017-06-16 15:26:33 -0400

    Loading message added. closes #2

--- models/forms.py
+++ models/forms.py
@@ -3,4 +3,4 @@ from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
 
 class url_form(Form):
-    url_field = StringField('url_field', validators=[DataRequired()])
+    url_field = StringField('url_field', validators=[DataRequired()], render_kw={"placeholder": "Enter band name."})
